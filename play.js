64e1ca776de75b24dd010bc68281d3b19f74e94e
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-23 22:10:56 +0000

    play

--- play.js
+++ play.js
@@ -1,18 +1,8 @@
 const net = require('net');
 const { connect } = require('./client')
+const { setupInput } = require('./input')
 console.log('Connecting ...');
-const setupInput = function() {
-const stdin = process.stdin;
-stdin.setRawMode(true);
-stdin.setEncoding('utf8');
-stdin.resume();
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-}};
-stdin.on ('data', handleUserInput);
-return stdin;
-}; 
+
    
 connect();
 setupInput();
