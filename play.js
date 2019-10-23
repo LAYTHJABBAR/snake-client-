28b643f706111df97f8eb4179eafbf34580dcfa4
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-23 22:01:14 +0000

    play

--- play.js
+++ play.js
@@ -2,15 +2,17 @@ const net = require('net');
 const { connect } = require('./client')
 console.log('Connecting ...');
 const setupInput = function() {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf8');
-    stdin.resume();
-    stdin.on ('data', (key) => {
-if (key === '\u0003') {
- process.exit();
-  }; return stdin;
-    });
-  };
-  connect();
-  setupInput();
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+stdin.resume();
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+}};
+stdin.on ('data', handleUserInput);
+return stdin;
+}; 
+   
+connect();
+setupInput();
